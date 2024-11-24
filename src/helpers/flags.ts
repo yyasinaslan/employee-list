e9fe3d3d41bed7d074d0f0a7bcde9d9073664b4c
YSN <yyasin.aslan@gmail.com>
2024-11-24 03:38:54 +0300

    fix: flag path

--- src/helpers/flags.ts
+++ src/helpers/flags.ts
@@ -1,3 +1,5 @@
+import {environment} from "../environment/environment.ts";
+
 export function getFlagUrl(locale: string, type: '4x3' | '1x1' = '4x3') {
-    return `/flags/${type}/${locale}.svg`;
+    return `${environment.baseUrl}/flags/${type}/${locale}.svg`;
 }
